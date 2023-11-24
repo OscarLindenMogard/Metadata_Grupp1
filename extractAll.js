82c3f8ca88cfdc220c79366f98551756f9ec0159
OscarLindenMogard <Oscar.Linden.data23sto@edu.tucsweden.se>
2023-11-24 14:57:13 +0100

    gör det bara lite snyggare

--- extractAll.js
+++ extractAll.js
@@ -7,8 +7,8 @@ console.log(images);
 
 for (let image of images) {
 
-    if(image.slice(-4) == '.jpg') {
-
+    if(image.slice(-4) == '.jpg') 
+    {
     console.log('image: ' + image)
     let metadata = await exifr.parse('bilder/' + image);
     console.log(metadata)
